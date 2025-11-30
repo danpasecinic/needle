7626be8b8660df391538123642ff000af1be7f20
danpasecinic <dan@withlocals.com>
2025-11-30 16:05:35 +0100

    refactor: unify metrics & health

--- observability.go
+++ observability.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+type ResolveHook func(key string, duration time.Duration, err error)
+
+type ProvideHook func(key string)
+
+type StartHook func(key string, duration time.Duration, err error)
+
+type StopHook func(key string, duration time.Duration, err error)
+
 type HealthStatus string
 
 const (
